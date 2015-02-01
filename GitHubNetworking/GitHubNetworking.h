c45cf337d365b6d46bb29e902bd644ffc324977d
Logan Wright <logan.william.wright@gmail.com>
2015-02-01 11:07:33 -0500

    Adding GitHubNetworking Header

---
+++ GitHubNetworking/GitHubNetworking.h
@@ -0,0 +1,28 @@
+//
+//  GitHubNetworking.h
+//  Pods
+//
+//  Created by Logan Wright on 2/1/15.
+//
+//
+
+#ifndef Pods_GitHubNetworking_h
+#define Pods_GitHubNetworking_h
+
+#import "GHNetworking.h"
+#import "GHAuth.h"
+#import "GHCredential.h"
+#import "GHRepo.h"
+
+#import "GHUser.h"
+#import "GHIssue.h"
+#import "GHIssueLabel.h"
+#import "GHIssueEvent.h"
+#import "GHIssueEventRename.h"
+#import "GHComment.h"
+#import "GHMilestone.h"
+#import "GHPartialPullRequest.h"
+
+#import "GHTransformers.h"
+
+#endif
